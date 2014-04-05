0335f3888fb80f71997628116cc6745c34e95885
Robert Massaioli <robertmassaioli@gmail.com>
2014-04-05 11:26:22 +1100

    Pretty print the contents of a riff file.
    Now it is time to push this repository somewhere safe and continue working on the
    structure of these files.

--- src/riff-structure.hs
+++ src/riff-structure.hs
@@ -2,10 +2,52 @@ module Main where
 
 import Data.Riff
 import System.Environment (getArgs)
+import Data.List (intersperse)
+import Control.Monad (when)
+import Data.Char (chr)
 
 main = do
    args <- getArgs
    case args of
       [] -> error "No filename provided to scan. Please give me a file."
-      x:_ -> withRiffFile x print
+      xs -> sequence_ . intersperse putNewline $ fmap processFile xs
 
+processFile :: String -> IO ()
+processFile filePath = do
+   putStr "File: "
+   print filePath
+   withRiffFile filePath (printRiffFile startContext)
+
+data PrintContext = PrintContext
+   { indentation :: Int
+   , printValue :: Bool
+   }
+   deriving (Show)
+
+startContext :: PrintContext
+startContext = PrintContext 0 False
+
+printRiffFile :: PrintContext -> RiffFile -> IO ()
+printRiffFile context (RiffChunkChild id size value) = do
+   printWithIndent context id
+   putStr $ " [" ++ show size ++ "]"
+   when (printValue context) $ do
+      putStr ": "
+      putStr $ fmap (chr . fromIntegral) value
+   putNewline
+printRiffFile context (RiffChunkParent id size typeName children) = do
+   printWithIndent context id
+   putStrLn $ " (" ++ typeName ++ ") [" ++ show size ++ "]"
+   mapM_ (printRiffFile nextContext) children
+   where
+      nextContext = PrintContext
+         { indentation = 1 + indentation context
+         , printValue = typeName == "INFO"
+         }
+
+printWithIndent :: PrintContext -> String -> IO ()
+printWithIndent context value = do
+   putStr . concat . take (indentation context) $ repeat "   "
+   putStr value
+
+putNewline = putStrLn ""
