78a412b24c8d7a7970e5287a3e7ecce9ad8f58a8
Robert Massaioli <robertmassaioli@gmail.com>
2014-04-06 13:33:53 +1000

    Finished documenting the entire module.

--- Data/Riff.hs
+++ Data/Riff.hs
@@ -1,10 +1,25 @@
-module Data.Riff 
-   ( RiffChunk(..)
-   , RiffFile(..)
-   , RiffFileType(..)
-   , RiffData
+{-|
+Description : The module that allows parsing of a RIFF / RIFX file.
+Copyright   : (c) Robert Massaioli, 2014
+License     : MIT
+Maintainer  : robertmassaioli@gmail.com
+Stability   : experimental
+
+This module allows the parsing of RIFF files in pure Haskell. You can parse a RIFF file using the 
+methods provided in this module. For example, if you wanted to parse a RIFF file and print it out
+then you could:
+
+> main = withRiffFile "path/to/file.riff" print
+
+And that will print the RIFF file, in gory details, to the screen.
+-}
+module Data.Riff
+   ( RiffFile(..)
    , RiffChunkSize
+   , RiffFileType(..)
+   , RiffChunk(..)
    , RiffId
+   , RiffData
    , ParseError
    , withRiffFile
    , parseRiffData
@@ -21,20 +36,38 @@ import Control.Monad (when)
 import Control.Monad.Trans.Either
 import Control.Monad.Trans.Class
 
+-- | The data in a riff file is just a collection of bytes.
 type RiffData = Word8
+
+-- | A Riff file is made up exclusively of Riff Chunks and each chunk, as the second piece
+-- of data in the chunk, contains it's size. The size never includes the first 8 bytes of
+-- the chunk, which are the Chunk Id and the Chunk Size but, in the case of a nested
+-- chunk, it does include the four bytes of the Chunk FormType Id.
+--
+-- According to the specification a chunk size must be represented by four bytes of data.
+-- This means that the maximum number of bytes that can be present in a RIFF file is 2 ^
+-- 32 + 8 = 4294967304 bytes or ~4GB. If your raw data is larger than that then this file
+-- format cannot support it.
 type RiffChunkSize = Word32
+
+-- | A RiffId is just a four character string (FourCC). It is usually (but by no means
+-- always) chosen to be something that is human readable when converted to ASCII. 
 type RiffId = String
 
+-- | Represents an error in the parsing of a Riff File. It contains the location in the
+-- file that we read up to and a message of what went wrong.
 type ParseError = (ByteOffset, String)
 
 -- | This is our representation of a RIFF file. These files all have a
 -- format type and are composed by one or more nestable data Chunks which we represent
 -- with a RiffChunk.
-data RiffFile = RiffFile
-   { riffFileType :: RiffFileType
-   , riffFileSize :: RiffChunkSize
-   , riffFileFormatType :: RiffId
-   , riffFileChildren :: [RiffChunk]
+data RiffFile = RiffFile 
+   { riffFileType :: RiffFileType      -- ^ The type of RIFF file.
+   , riffFileSize :: RiffChunkSize     -- ^ The size of all the contained data. This does not include
+                                       -- four bytes for the id and four bites for this
+                                       -- size itself.
+   , riffFileFormatType :: RiffId      -- ^ An ID representing the type of data contained within.
+   , riffFileChildren :: [RiffChunk]   -- ^ The chunks that make up the file.
    }
    deriving (Eq, Show)
 
@@ -74,7 +107,11 @@ withRiffFile filePath action = withBinaryFile filePath ReadMode $ \h -> do
    riffData <- fmap parseRiffData (BL.hGetContents h)
    action riffData
 
-parseRiffData :: BL.ByteString -> Either ParseError RiffFile
+-- | You can parse a raw ByteString and try and convert it into a RiffFile. This will give
+-- a best attempt at parsing the data and, if success is not possible, will give you a
+-- ParseError.
+parseRiffData :: BL.ByteString               -- A lazy bytestring for input.
+              -> Either ParseError RiffFile  -- The result of our attempted parse.
 parseRiffData input =
    case runGetOrFail (runEitherT getRiffStart) input of
       Left (_, offset, error) -> Left (offset, error)
